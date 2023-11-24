74949684961524eee59c08b513a8d2b9f29d3e4e
alemishch <mishchenko.ad@phystech.edu>
2023-11-24 12:52:21 +0300

    demo

---
+++ drive.py
@@ -0,0 +1,88 @@
+import RPi.GPIO as GPIO          
+import time
+
+en1 = 26
+en2 = 22
+en3 = _
+en4 = _
+in1_1 = 19
+in1_2 = 13
+in2_1 = 6
+in2_2 = 5
+in3_1 = 6
+in3_2 = 5
+in4_1 = 6
+in4_2 = 5
+
+orient = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1_1,GPIO.OUT)
+GPIO.setup(in1_2,GPIO.OUT)
+GPIO.setup(in2_1,GPIO.OUT)
+GPIO.setup(in2_2,GPIO.OUT)
+GPIO.setup(en1,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
+GPIO.setup(en3,GPIO.OUT)
+GPIO.setup(en4,GPIO.OUT)
+GPIO.output(in1_1,GPIO.LOW)
+GPIO.output(in1_2,GPIO.LOW)
+GPIO.output(in2_1,GPIO.LOW)
+GPIO.output(in2_2,GPIO.LOW)
+GPIO.output(in3_1,GPIO.LOW)
+GPIO.output(in3_2,GPIO.LOW)
+GPIO.output(in4_1,GPIO.LOW)
+GPIO.output(in4_2,GPIO.LOW)
+p1=GPIO.PWM(en1,1000)
+p2=GPIO.PWM(en2,1000)
+p1=GPIO.PWM(en3,1000)
+p2=GPIO.PWM(en4,1000)
+p1.start(0)
+p2.start(0)
+p3.start(0)
+p4.start(0)
+
+motors = [(in1_1, in1_2, en1), (in2_1, in2_2, en2), (in3_1, in3_2, en3), (in4_1, in4_2, en4)]
+
+def setSpeed(motor, speed):
+	if speed == 0:
+		GPIO.output(motor[0] ,GPIO.LOW)
+		GPIO.output(motor[1] ,GPIO.LOW)
+	elif speed > 0:
+		GPIO.output(motor[0] ,GPIO.HIGH)
+		GPIO.output(motor[1] ,GPIO.LOW)
+		motor[3].ChangeDutyCycle(speed)
+	else:
+		GPIO.output(motor[0] ,GPIO.LOW)
+		GPIO.output(motor[1] ,GPIO.HIGH)
+		motor[3].ChangeDutyCycle(speed)
+		
+
+def move_forward(speed, time, motors):
+	start = time.time()
+	for motor in motors:
+			setSpeed(motor, speed)
+	while time.time() < time:
+		continue
+	for motor in motors:
+			setSpeed(motor, 0)
+			
+			
+	
+	
+def rotate_90(time, where, motors, speed):
+	time = time.time()
+	if where == 'left':
+		setSpeed(motors[0], 0)
+		setSpeed(motors[2], 0)
+		setSpeed(motors[1], speed)
+		setSpeed(motors[3], -speed)
+	elif where == 'right':
+		setSpeed(motors[1], 0)
+		setSpeed(motors[2], 0)
+		setSpeed(motors[0], speed)
+		setSpeed(motors[3], -speed)
+		for motor in motors:
+			setSpeed(motor, 0)
+	
+		
---
+++ webcam_digit.py
@@ -0,0 +1,100 @@
+import cv2
+import time
+from picamera2 import Picamera2
+from keras.models import model_from_json
+import numpy as np
+
+cam = Picamera2()
+fps = 30
+cam.preview_configuration.main.size = (640, 360)
+cam.preview_configuration.main.format = "RGB888"
+cam.preview_configuration.controls.FrameRate=30
+cam.preview_configuration.align()
+cam.configure("preview")
+cam.start()
+
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights("model.h5")
+print("Loaded model from disk")
+
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    
+###cap = cv2.VideoCapture(0)
+
+    
+def getIm(img_processed, img_raw, x, y, h, w):
+    ret, thresh = cv2.threshold(blur, 175, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    newImage = thresh[y:y + h, x:x + w] #saving threshold image into new image
+    #resizing the image into 28*28. using opencv to resize image without distortion. 
+    newImage = cv2.resize(newImage, (28, 28))
+    # converting images to numpy array of images
+    newImage = np.array(newImage)
+    newImage = newImage.astype('float32')
+    # normalizing the image 
+    newImage /= 255
+    # reshaping numpy array of images. numpy.reshape just change the shape attribute without changing the data (images) at all.
+    newImage = newImage.reshape(28, 28, 1)
+    # inserting a new axis at the 0th location to expand the size of numpy array of images of 4 from (28,28,1) to (1,28,28,1)
+    newImage = np.expand_dims(newImage, axis=0)
+    ans = ''
+    
+    # predicting the model    
+    ans = model.predict(newImage, verbose=0).argmax()
+    
+    #putText(img, text, org(it is a point representing the bottom left corner text string in the image), fontFace, fontScale, Scalar color, int thickness)
+    cv2.putText(img, "CNN : " + str(ans), (10, 320),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    
+    return ans    
+    
+def GetAngle(img_processed, img_raw, x, y, h, w):
+    edges = cv2.Canny(img_processed, 50, 150)[y:y + h, x:x + w]
+    rho = 1  # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 80  # minimum number of pixels making up a line
+    max_line_gap = 20  # maximum gap in pixels between connectable line segments
+
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                        min_line_length, max_line_gap)
+    if lines is not None:
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                cv2.line(img_raw,(x1,y1),(x2,y2),(255,0,0),5)
+    
+    
+while True:
+    ###ret, img = cap.read()
+    
+    tStart=time.time()
+
+    img=cam.capture_array()
+    
+    x, y, w, h = 0, 0, 300, 300 
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+    
+    num = getIm(blur, img, x, y, h, w)
+    GetAngle(blur, img, x, y, h, w)
+    
+    cv2.imshow("Frame", img)
+    
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+        
+    tEnd=time.time()
+    # time between two epochs
+    looptime=tEnd-tStart
+    fps=1/looptime
+    
+    
+cv2.destroyAllWindows()
+###cap.release()
+cam.stop()
