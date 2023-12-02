f1b81486ab12908cf20dec6105fe30647e8554c2
alemishch <mishchenko.ad@phystech.edu>
2023-12-02 21:24:23 +0300

    working

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ en1 = 19
 en2 = 12
 en3 = 18
 en4 = 13
-in1_1 = 26
-in1_2 = 6
-in2_1 = 5
-in2_2 = 0
+in1_2 = 26
+in1_1 = 6
+in2_2 = 5
+in2_1 = 0
 in3_1 = 20
 in3_2 = 16
 in4_1 = 1
@@ -35,86 +35,7 @@ print("Loaded model from disk")
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
     
 ###cap = cv2.VideoCapture(0)
-
-    
-def getDigit(img_processed, img_raw, x, y, h, w):
-    ret, thresh = cv2.threshold(img_processed, 175, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    newImage = thresh[y:y + h, x:x + w] 
-    newImage = cv2.resize(newImage, (28, 28))
-    newImage = np.array(newImage)
-    newImage = newImage.astype('float32')
-    newImage /= 255
-    newImage = newImage.reshape(28, 28, 1)
-    newImage = np.expand_dims(newImage, axis=0)
-    ans = ''
-    ans = model.predict(newImage, verbose=0).argmax()
-    
-    cv2.putText(img_raw, "CNN : " + str(ans), (10, 320),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                    
-    return ans  
-    
-def GetAngle(img_processed, img_raw, x, y, h, w):
-    edges = cv2.Canny(img_processed, 50, 150)[y:y + h, x:x + w]
-    rho = 1  # distance resolution in pixels of the Hough grid
-    theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-    min_line_length = 80  # minimum number of pixels making up a line
-    max_line_gap = 20  # maximum gap in pixels between connectable line segments
-
-    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                        min_line_length, max_line_gap)
-
-    lengths = []
-    angles = []
-    if lines is not None:
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                cv2.line(img_raw,(x1,y1),(x2,y2),(255,0,0),5)
-                lengths.append(np.sqrt((x1-x1)**2+(y1-y2)**2))
-                if y1>y2:                                           ########test later
-                    dx = x1-x2
-                else:
-                    dx = x2-x1
-                angles.append(180/np.pi*np.arctan(dx/np.abs(y1 - y2))) ############# test zero
-    ind = np.argsort(lengths)
-    
-    if len(ind) < 2:
-        return 0, 0
-    else:
-        angle1 = angles[ind[0]]
-        angle2 = angles[ind[1]]
-        for i in range(1, len(ind)):
-            if np.abs(np.abs(angles[ind[i]]) - np.abs(angle1)) >10:
-                angle2 = angles[ind[i]]
         
-        return angle1, angle2
-        
-        
-def videoLoop():
-	while True:
-		###ret, img = cap.read()
-
-
-		img=cam.capture_array()
-		
-		x, y, w, h = 0, 0, 300, 300 
-		
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		blur = cv2.GaussianBlur(gray, (5, 5), 0)
-		
-		cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
-		
-		num = getDigit(blur, img, x, y, h, w)
-		angles = GetAngle(blur, img, x, y, h, w)
-		#print(angles)
-		
-		cv2.imshow("Frame", img)
-				
-		c = cv2.waitKey(1)
-		if c == 27:
-			break
-
     
 class Motor:
 	def __init__(self, in1, in2, en):
@@ -158,21 +79,81 @@ class Cart:
 		self.cam.configure("preview")
 		self.cam.start()
 		
+		self.angles = (1, 1)
+		self.x, self.y, self.w, self.h = 0, 0, 300, 300 
+		
 	def image(self):	
 		self.img=self.cam.capture_array()
 		
-		x, y, w, h = 0, 0, 300, 300 
-		
 		gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-		blur = cv2.GaussianBlur(gray, (5, 5), 0)
+		self.blur = cv2.GaussianBlur(gray, (5, 5), 0)
 		
-		cv2.rectangle(self.img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+		cv2.rectangle(self.img, (self.x, self.y), (self.x + self.w, self.y + self.h), (255, 255, 255), 2)
 		
-		self.digit = getDigit(blur, self.img, x, y, h, w)
-		self.angles = GetAngle(blur, self.img, x, y, h, w)
+		self.getDigit()
+		self.GetAngle()
 		
 		cv2.imshow("Frame", self.img)
-				
+		
+		
+	def getDigit(self):
+		ret, thresh = cv2.threshold(self.blur, 175, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+		newImage = thresh[self.y:self.y + self.h, self.x:self.x + self.w] 
+		newImage = cv2.resize(newImage, (28, 28))
+		newImage = np.array(newImage)
+		newImage = newImage.astype('float32')
+		newImage /= 255
+		newImage = newImage.reshape(28, 28, 1)
+		newImage = np.expand_dims(newImage, axis=0)
+		ans = ''
+		ans = model.predict(newImage, verbose=0).argmax()
+		
+		cv2.putText(self.img, "CNN : " + str(ans), (10, 320),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+						
+		self.digit = ans 
+    
+	def GetAngle(self):
+		edges = cv2.Canny(self.blur, 50, 150)[self.y:self.y + self.h, self.x:self.x + self.w]
+		rho = 1  # distance resolution in pixels of the Hough grid
+		theta = np.pi / 180  # angular resolution in radians of the Hough grid
+		threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+		min_line_length = 80  # minimum number of pixels making up a line
+		max_line_gap = 20  # maximum gap in pixels between connectable line segments
+
+		lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+							min_line_length, max_line_gap)
+
+		lengths = []
+		angles = []
+		if lines is not None:
+			for line in lines:
+				for x1,y1,x2,y2 in line:
+					cv2.line(self.img,(x1,y1),(x2,y2),(255,0,0),5)
+					
+					lengths.append(np.sqrt((x1-x1)**2+(y1-y2)**2))
+					if y1>y2:                                           ########test later
+						dx = x1-x2
+						angles.append(180/np.pi*np.arctan(dx/np.abs(y1 - y2)))
+					elif y1 < y2:
+						dx = x2-x1
+						angles.append(180/np.pi*np.arctan(dx/np.abs(y1 - y2)))
+					elif y1 == y2:
+						angles.append(90)
+						
+		ind = np.argsort(lengths)
+		print(angles)
+		if len(ind) < 2:
+			self.angles = [1, 1]
+		else:
+			angle1 = angles[ind[-1]]
+			angle2 = angles[ind[-2]]   ################
+			for i in range(1, len(ind)):
+				if np.abs(np.abs(angles[ind[i]]) - np.abs(angle1)) >10:
+					angle2 = angles[ind[i]]
+					break
+			
+			self.angles = [angle1, angle2]
 		
 	
 	def moveForward(self, speed, Time):
@@ -188,10 +169,8 @@ class Cart:
 			motor.setSpeed(0)		
 	
 	
-	def rotate90(self, where):
-		Time = 2 ######
-		speed = 0.2 #####
-		start = time.Time()
+	def rotate(self, where, speed, Time):
+		start = time.time()
 		if where == 'left':
 			self.fl.setSpeed(0)
 			self.br.setSpeed(0)
@@ -210,9 +189,48 @@ class Cart:
 		for motor in self.motors:
 			motor.setSpeed(0)
 			
+			
+	def stay(self, Time):
+		print('staying')
+		start = time.time()
+		for motor in self.motors:
+			motor.setSpeed(0)
+		if Time > 0:
+			while time.time() - start < Time:
+				print(self.angles)
+				self.image()
+				c = cv2.waitKey(1)
+				if c == 27:
+					break
+		else:
+			while True:
+				self.image()
+				c = cv2.waitKey(1)
+				if c == 27:
+					break
+			
+			
 
-		def rotateAngle(self, angle):
-			pass
+	def rotateAngle(self):
+		while True:	
+			self.image()
+			
+			c = cv2.waitKey(1)
+			if c == 27:
+				break
+				
+			print(self.angles)
+			ind = np.argsort(np.abs(self.angles))
+			angle = self.angles[ind[0]]
+			if np.abs(angle) < 0.5:
+				print('rotated')###############
+				break
+			
+			if angle >= 0:
+				self.rotate('left', 0.02, 0.005)
+			else:
+				self.rotate('right', 0.1, 0.01)
+			
 			
 
 	
@@ -221,12 +239,27 @@ m2 = Motor(in2_1, in2_2, en2)
 m3 = Motor(in3_1, in3_2, en3)
 m4 = Motor(in4_1, in4_2, en4)
 
-cart = Cart(m1, m2, m3, m4)
+cart = Cart(m1, m2, m4, m3)
 
+cart.stay(5)
+cart.rotateAngle()
+cart.stay(0)
 
-cart.moveForward(0.5, 20)
-	
+#cart.moveForward(0.1, 5)
+#time.sleep(10)
+#cart.stay(30)
+#cart.moveForward(0.05, 1)
+#cart.rotate('right', 0.1, 0.58)       ###############
+#cart.stay(2)
+#cart.moveForward(0.05, 0.5)
+#cart.rotate('left', 0.1, 0.78)         #############
 
-#cv2.destroyAllWindows()
+#cart.br.setSpeed(0.1)
+#time.sleep(1)
+#cart.br.setSpeed(-0.1)
+#time.sleep(1)
+#cart.br.setSpeed(0)			
+
+cv2.destroyAllWindows()
 ###cap.release()
-#cam.stop()
+cart.cam.stop()
