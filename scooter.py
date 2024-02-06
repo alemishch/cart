2fe0491642d34e07c34b5582f219d651d6029f0d
alemishch <mishchenko.ad@phystech.edu>
2024-02-07 01:30:07 +0300

    added red color enhansing

--- scooter.py
+++ scooter.py
@@ -55,27 +55,18 @@ def find_intersection(x_start1, y_start1, x_end1, y_end1, x_start2, y_start2, x_
 
 	return (x, y)
 
-def normalizeBrightness(image):
-	if image is None:
-	    print("Error: Unable to load the image.")
-	    return None
+def enhance_red_color(image, saturation_factor=2.0):
+	hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	h_channel, s_channel, v_channel = cv2.split(hsv_image)
 
-	lower_bound = np.array([0, 0, 200], dtype=np.uint8)
-	upper_bound = np.array([255, 255, 255], dtype=np.uint8)
+	s_channel[s_channel > 60] = np.clip(s_channel[s_channel > 60] * saturation_factor, 0, 255)
 
-	mask = cv2.inRange(hsv, lower_bound, upper_bound)
-	brightness_adjustment = -50
-	image[mask > 0] = np.clip(image[mask > 0].astype(int) + brightness_adjustment, 0, 255).astype(np.uint8)
+	enhanced_hsv_image = cv2.merge((h_channel, s_channel, v_channel))
 
-	inverted_mask = cv2.bitwise_not(mask)
-	brightness_adjustment = 50
-	image[inverted_mask > 0] = np.clip(image[inverted_mask > 0].astype(int) + brightness_adjustment, 0, 255).astype(np.uint8)
+	enhanced_image = cv2.cvtColor(enhanced_hsv_image, cv2.COLOR_HSV2BGR)
 
-	result = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
-
-	return result
+	return enhanced_image
 
 def is_point_inside_square(point, square_size, window_size):
 
@@ -176,7 +167,7 @@ class Cart:
 		
 	def image(self):	
 		self.img=self.cam.capture_array()
-		self.img = normalizeBrightness(self.img)
+		self.img = enhance_red_color(self.img)
 		
 		hsv_image = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
 		#lower_red = np.array([0, 50, 50])
